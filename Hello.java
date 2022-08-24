f6c66b3df3f379ce8fc390a5328aeb5778c7112b
Bkemmey <112005235+Bkemmey@users.noreply.github.com>
2022-08-24 12:24:11 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/***********************
+* Hello World
+************************
+* Bryce Kemmey
+* 08/24/22
+* 255 04
+************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("Bryce Kemmey");
+		System.out.println("<Hello.java>");
+		System.out.println("Lab 1");
+		System.out.println("08/24/2022");
+		System.out.println("255 04");
+		System.out.println();
+	}
+}
